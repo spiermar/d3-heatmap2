abd84fd7747bd0ac9d7135d9c9e4a3ae3e6cf24a
Martin Spier <spiermar@gmail.com>
2018-02-17 22:03:56 -0800

    task: adding lint and browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,59 @@
 var gulp = require('gulp');
 var rollup = require('rollup-stream');
 var source = require('vinyl-source-stream');
+var uglify = require('gulp-uglify');
+var del = require('del');
+var rename = require('gulp-rename');
+var eslint = require('gulp-eslint');
+var browserSync = require('browser-sync').create();
+
+gulp.task('clean', function() {
+    return del(['dist']);
+});
+
+gulp.task('lint', function() {
+    return gulp.src(['./src/**/*.js'])
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError());
+});
 
 gulp.task('rollup', function() {
-    
-  });
-
-function rollupTask() {
     return rollup({
         input: './index.js',
         name: 'd3',
         format: 'umd',
         extend: true,
         sourcemap: false
-      })
-      .pipe(source('d3-heatmap.js'))
-      .pipe(gulp.dest('./dist'));
-};
+    })
+        .pipe(source('d3-heatmap.js'))
+        .pipe(gulp.dest('./dist'));
+});
 
-function watchTask() {
-    gulp.watch('./src/**/*.js', ['rollup']);
-}
+gulp.task('uglify', function() {
+    return gulp.src('./dist/d3-heatmap.js')
+        .pipe(gulp.dest('./dist'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist'));
+});
 
-exports.rollup = rollupTask;
-exports.watch = watchTask;
+gulp.task('style', function () {
+    return gulp.src('./src/heatmap.css')
+        .pipe(rename("d3-heatmap.css"))
+        .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: ['examples', 'dist']
+        }
+    });
+});
+
+gulp.task('dev', gulp.series('browser-sync', function () {
+    gulp.watch("./src/**/*.js", gulp.series('lint', 'rollup'));
+}));
+
+gulp.task('build', gulp.series('clean', 'lint', 'rollup', 'uglify', 'style'));
